a3a3dbf5c8ec4c57527b085a8e5ec2e56720177e
JavieroXD <147724977+JavieroXD@users.noreply.github.com>
2025-02-20 20:08:24 +0700

    Create FindMap.lua

---
+++ FindMap.lua
@@ -0,0 +1,19 @@
+if game.PlaceId == 394506555 then -- glue piece
+    if getgenv().lang == "EN" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JavieroXD/Yze-hub/refs/heads/main/Glue-piece/eng.lua"))()
+    elseif getgenv().lang == "TH" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JavieroXD/Yze-hub/refs/heads/main/Glue-piece/thai.lua"))(
+    else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Error", -- Required
+	Text = "Language not found! use EN or TH", -- Required
+	Icon = "rbxassetid://112158874230328" -- Optional
+})
+    end
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Error", -- Required
+	Text = "Game not supported.", -- Required
+	Icon = "rbxassetid://112158874230328" -- Optional
+})
+end
