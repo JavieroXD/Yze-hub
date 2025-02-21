7e6f7bf9959c3485c32c785331350d695124eaf4
JavieroXD <147724977+JavieroXD@users.noreply.github.com>
2025-02-21 21:38:36 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,5 @@
-local lang = getgenv().lang or "EN"
 if game.PlaceId == 394506555 then -- glue piece
-    if lang == "EN" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JavieroXD/Yze-hub/refs/heads/main/Glue-piece/eng.lua"))()
-    elseif lang == "TH" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JavieroXD/Yze-hub/refs/heads/main/Glue-piece/thai.lua"))()
-    else
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Error", -- Required
-	Text = "Language not found! use EN or TH", -- Required
-	Icon = "rbxassetid://112158874230328" -- Optional
-})
-    end
 else
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Error", -- Required
